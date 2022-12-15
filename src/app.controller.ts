19b3744b7a2574cc59c9b9d22f67b6558908f8a8
antonio-cosmo <antoniocosmosilvaneto@gmail.com>
2022-12-15 10:38:51 -0300

    notification creation request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateNotificationDto } from './dto/create-notification.dto';
 
 @Controller('notifications')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get()
+  async findAll() {
+    return await this.appService.findAll();
+  }
+
+  @Post()
+  async create(@Body() data: CreateNotificationDto){
+    const {recipientId, content, category} = data
+   await this.appService.create({recipientId, content, category});
+  }
  
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { randomUUID } from 'crypto';
+import { CreateNotificationDto } from './dto/create-notification.dto';
 import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  
+  async findAll() {
+    return await this.prismaService.notification.findMany();
+  }
+
+  async create({recipientId, content, category}: CreateNotificationDto){
+    await this.prismaService.notification.create({data:{
+      id: randomUUID(),
+      content,
+      category,
+      recipientId
+    }})
+  }
 }
