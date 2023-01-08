db10c950245847c0347c4fb94d146d10f75d5095
antonio-cosmo <antoniocosmosilvaneto@gmail.com>
2023-01-08 15:03:07 -0300

    swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { KafkaConsumerService } from '@infra/messaging/kafka/kafka-consumer.serv
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions } from '@nestjs/microservices';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   const kafkaConsumerService = app.get(KafkaConsumerService);
@@ -15,6 +20,17 @@ async function bootstrap() {
   });
 
   await app.startAllMicroservices();
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Notifications Service')
+    .setDescription('API Microsservice notifications')
+    .setVersion('1.0')
+    .build();
+
+  const documentSwagger = SwaggerModule.createDocument(app, configSwagger);
+
+  SwaggerModule.setup('api', app, documentSwagger);
+
   await app.listen(3000);
 }
 bootstrap();
